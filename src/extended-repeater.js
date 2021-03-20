0aea1709803194916e91e0121d8979f2e4bf74d2
mishafuod-13 <mishafuod@gmail.com>
2021-03-20 21:29:25 +0300

    feat: add solution for extended-repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,66 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+  let resstr = "";
+
+  let ch=1;
+  
+  if (options.repeatTimes!==undefined){
+    ch = options.repeatTimes;
+  }
+
+  let separator = "+";
+  if (options.separator!==undefined){
+    separator = options.separator;
+  }
+  
+
+  function addition () {
+    let res = "";
+      if (options.addition!==undefined){
+        res+=options.addition; 
+      }
+
+    let sep = "|";
+
+      if (options.additionSeparator!==undefined){
+        sep=options.additionSeparator; 
+      }
+
+        let repeat = 1;
+        if (options.additionRepeatTimes!==undefined){
+          repeat=options.additionRepeatTimes;
+        }
+
+        if (repeat==1) {
+          return res
+        }
+
+        let fres = "";
+        for (let i = 1; i < repeat; i++) {
+          fres += res+sep;
+          if (i===repeat-1){
+            fres+=res;     
+          }
+          
+  
+        }
+      
+    return fres;
+
+  }
+
+  let add = addition();
+  
+    for (let i = 0; i < ch; i++) {
+      if (i===0) {
+        resstr += str+add;
+      } else {
+        resstr += separator+str+add;
+      }
+    }
+
+    return resstr;
 };
   
